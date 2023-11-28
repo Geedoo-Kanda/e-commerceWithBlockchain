188d27facc5f033d2776f2d0409f6fa12ed119fb
Geedoo Kanda <geedookanda06@gmail.com>
2023-11-28 23:30:59 +0100

    gestion du contrat Produit

---
+++ contracts/ProduitRepository.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+contract ProductRepository {
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        string description;
+        string category;
+        string image;
+    }
+
+    mapping(uint => Product) public products;
+    uint public nextId = 1;
+
+    event ProductCreated(
+        uint indexed id,
+        string name,
+        uint price,
+        string description,
+        string category,
+        string image
+    );
+
+    function createProduct(
+        string memory _name,
+        uint _price,
+        string memory _description,
+        string memory _category,
+        string memory _image
+    ) public {
+        Product memory newProduct = Product(
+            nextId,
+            _name,
+            _price,
+            _description,
+            _category,
+            _image
+        );
+        products[nextId] = newProduct;
+
+        emit ProductCreated(
+            nextId,
+            _name,
+            _price,
+            _description,
+            _category,
+            _image
+        );
+
+        nextId++;
+    }
+
+    function getProductById(uint _id) public view returns (Product memory) {
+        return products[_id];
+    }
+
+    function listProducts() public view returns (Product[] memory) {
+        Product[] memory productList = new Product[](nextId);
+
+        for (uint i = 0; i < nextId; i++) {
+            productList[i] = products[i];
+        }
+
+        return productList;
+    }
+}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const UserRepository = artifacts.require("UserRepository");
+const ProduitRepository = artifacts.require("ProduitRepository");
 
 module.exports = function(deployer) {
    deployer.deploy(UserRepository);
+   deployer.deploy(ProduitRepository);
 };
